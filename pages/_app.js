fc1cf1003912b1c74455f457d058b40065eca5bd
BekhaIrga <irgashevbehzod@gmail.com>
2021-03-13 16:30:36 +0500

    getInitialProps from _App.js in order to send props to other components

--- pages/_app.js
+++ pages/_app.js
@@ -4,44 +4,59 @@ import Head from "next/head";
 import React from "react";
 import App from "next/app";
 import Footer from "../components/footer";
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
-        <script
-          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-          crossOrigin="anonymous"
-        ></script>
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-          crossOrigin="anonymous"
-        ></script>
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <Navbar />
-      <div className="base-page">
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-      <style jsx>{`
+class MyApp extends App{
+    constructor(props) {
+        super(props);
+    }
+    static async getInitialProps(appContext){
+        //App.getInitialProps calls the methods getInitialProps from other components that are existing
+        //and then sending them to the <Component /> that is being rendered or where we are navigating
+        const appProps = await App.getInitialProps(appContext);
+        return {
+            ...appProps,
+        }
+    }
+  render(){
+      const {Component, pageProps} = this.props;
+      return (
+          <>
+              <Head>
+                  <title>Home</title>
+                  <link
+                      rel="stylesheet"
+                      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                      crossOrigin="anonymous"
+                  />
+                  <script
+                      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                      crossOrigin="anonymous"
+                  ></script>
+                  <script
+                      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                      crossOrigin="anonymous"
+                  ></script>
+                  <script
+                      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                      crossOrigin="anonymous"
+                  ></script>
+              </Head>
+              <Navbar />
+              <div className="base-page">
+                  <Component {...pageProps} />
+              </div>
+              <Footer />
+              <style jsx>{`
         .base-page {
           padding-top: 55px;
         }
       `}</style>
-    </>
-  );
+          </>
+      );
+  }
+
 }
 export default MyApp;
