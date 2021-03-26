0c284b1a56968931c3b49cb3a0e932f23c67ad28
BekhaIrga <irgashevbehzod@gmail.com>
2021-03-26 11:04:09 -0700

    Added close event for create movie

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -2,15 +2,17 @@ import Modal from './modal';
 import MovieCreateForm from './movieCreateForm';
 import { createMovie } from '../actions/index';
 const SideMenu = ({ categories }) => {
+	let modalRef = null;
 	const handleCreateMovie = (movie) => {
 		createMovie(movie).then((movies) => {
 			console.log(JSON.stringify(movies));
+			modalRef.closeModal();
 		});
 	};
 
 	return (
 		<>
-			<Modal hasSubmit={false}>
+			<Modal hasSubmit={false} ref={(el) => (modalRef = el)}>
 				<MovieCreateForm handleCreateMovie={handleCreateMovie} />
 			</Modal>
 			<h1 className='my-4'>Shop Name</h1>
