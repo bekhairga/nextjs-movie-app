52ef110ff03b42f1d5ad84c7d5e698081d630a45
BekhaIrga <irgashevbehzod@gmail.com>
2021-04-10 13:20:16 -0700

    Custom api second usage without server

--- pages/api/v1/posts.js
+++ pages/api/v1/posts.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default async (req, res) => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    const posts = response.data;
-    return res.json(posts.slice(0, 20));
-}
+	const response = await axios.get(
+		'https://jsonplaceholder.typicode.com/posts'
+	);
+	const posts = response.data;
+	return res.json(posts.slice(0, 20));
+};
